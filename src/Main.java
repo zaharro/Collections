a74042f37833f31bf1cb1a32bbdf32ba3d14cf8b
Nikolay Zakharin <nikolayzaharyin@gmail.com>
2025-03-18 13:45:03 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,70 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
+  Найти и вывести список уникальных слов, из которых состоит массив.
+   Посчитать, сколько раз встречается каждое слово*/
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+   public static void main(String[] args) {
+
+       ArrayList<String> words = new ArrayList<>();
+        words.add("Book");
+        words.add("Joke");
+        words.add("Tree");
+        words.add("Drink");
+        words.add("Book");
+        words.add("Tree");
+        words.add("Tree");
+        words.add("People");
+        words.add("Product");
+        words.add("Product");
+        words.add("Product");
+        words.add("Product");
+        words.add("Coke");
+        words.add("Joke");
+        words.add("Game");
+        words.add("Shame");
+        words.add("Name");
+        words.add("Object");
+        words.add("Picture");
+        words.add("Picture");
+
+        System.out.println("Первоначальный список слов, из которых состоит массив: ");
+        //words.forEach(System.out::println);
+        System.out.println(words);
+
+        System.out.println("Список уникальных слов, из которых состоит массив: ");
+        HashSet<String> uniqueWords = new HashSet<>(words);
+        //uniqueWords.forEach(System.out::println);
+        System.out.println(uniqueWords);
+
+        System.out.println("Каждое слово встречается следующее количество раз:");
+
+        Map<String, Integer> wordsCount = new HashMap<>();
+        for (String word : words) {
+            wordsCount.put(word, wordsCount.getOrDefault(word, 0) + 1);
         }
+        //wordsCount.entrySet().forEach(System.out::println);
+        //System.out.println(wordsCount);
+        System.out.println(wordsCount.entrySet());
+
+
+       TelephoneDirectory directory = new TelephoneDirectory();
+
+       directory.add("Petrov", "89111234567");
+       directory.add("Petrov", "89111234568");
+       directory.add("Sidorov", "89111234569");
+       directory.add("Sidorov", "89111234570");
+       directory.add("Ivanov", "89311534567");
+
+       directory.get("Petrov");
+       directory.get("Ivanov");
+       directory.get("Sidorov");
+       directory.get("Pavlov");
     }
-}
+
+   }
+
+
--- src/TelephoneDirectory.java
+++ src/TelephoneDirectory.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+/*2. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий
+и телефонных номеров. В этот телефонный справочник с помощью метода add() можно добавлять записи,
+а с помощью методы get() искать номер телефона по фамилии.
+Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
+тогда при запросе такой фамилии должны выводиться все телефоны.
+Не забыть про использование интерфейса.*/
+
+import java.util.*;
 
 public class TelephoneDirectory {
+
+    private final Map<String, ArrayList<String>> items = new HashMap<>();
+
+    public void add(String surname, String phoneNumber) {
+
+        if (items.get(surname) != null) {
+
+            if (!items.get(surname).contains(phoneNumber))
+                items.get(surname).add(phoneNumber);
+        } else {
+            ArrayList<String> numberList = new ArrayList<>();
+            numberList.add(phoneNumber);
+            items.put(surname, numberList);
+        }
+    }
+
+    public void get(String surname) {
+        if (items.get(surname) != null)
+            System.out.printf("Телефон контакта %s: %s\n", surname, items.get(surname));
+        else
+            System.out.printf("Телефон контакта %s не найден\n", surname);
+    }
+
 }
